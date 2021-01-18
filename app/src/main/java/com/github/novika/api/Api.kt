4d33c60a9758dd212be562e251943a4b85022bb2
Novikasari K <44634056+novikasari@users.noreply.github.com>
2021-01-18 11:07:34 +0700

    Update Api.kt
    update token

--- app/src/main/java/com/github/novika/api/Api.kt
+++ app/src/main/java/com/github/novika/api/Api.kt
@@ -11,25 +11,25 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token 41476a65f44abcbd84614788e8c3b975db092044")
+    @Headers("Authorization: token e56d46becbc4a63a364dcd13bf9496e0d23e0921")
     fun getSearchUsers(
         @Query("q")query : String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 41476a65f44abcbd84614788e8c3b975db092044")
+    @Headers("Authorization: token e56d46becbc4a63a364dcd13bf9496e0d23e0921")
     fun getUserDetail(
         @Path("username") username : String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 41476a65f44abcbd84614788e8c3b975db092044")
+    @Headers("Authorization: token e56d46becbc4a63a364dcd13bf9496e0d23e0921")
     fun getFollowers(
         @Path("username") username : String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 41476a65f44abcbd84614788e8c3b975db092044")
+    @Headers("Authorization: token e56d46becbc4a63a364dcd13bf9496e0d23e0921")
     fun getFollowing(
         @Path("username") username : String
     ): Call<ArrayList<User>>
